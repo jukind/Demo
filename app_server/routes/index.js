bf8d83e7d600bec89cf4222c9896617f525658dd
jukind <38502097+jukind@users.noreply.github.com>
2018-12-02 19:56:04 +0200

    Added all the demo files

---
+++ app_server/routes/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+// Declaring the controllers
+const ctrlMain = require('../controllers/ctrlMain'); // Controller for the Home page
+const ctrlmajor = require('../controllers/ctrlmajor'); // ...
+const ctrlmvp = require('../controllers/ctrlmvp'); // ...
+
+// Defining the routes and associating the routes to their corresponding controllers
+router.get('/', ctrlMain.index);  // Home page
+router.get('/majorWinners', ctrlmajor.winnerlist);  //
+router.get('/mvp', ctrlmvp.winnerlist);  //
+
+router
+	.route('/majorWinners/add')
+	.get(ctrlmajor.showForm)
+	.post(ctrlmajor.addData);
+
+module.exports = router;
